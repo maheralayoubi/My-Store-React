624f24f6eb89701b600a783e15ce4c8ee34381c3
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-09-27 17:52:14 +0900

    Updating modules

---
+++ build/precache-manifest.7a24617b908e4b3bdf6b06c1940fa0ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8280151fb6913f40439baa56b659bb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba6961b1956986dfc23",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "32e4822d8008ad8121e4",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4ba6961b1956986dfc23",
+    "url": "/static/js/2.cd9f3069.chunk.js"
+  },
+  {
+    "revision": "32e4822d8008ad8121e4",
+    "url": "/static/js/main.2bc9ee50.chunk.js"
+  },
+  {
+    "revision": "4356a34802b838573202",
+    "url": "/static/js/runtime~main.e6dfe110.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);

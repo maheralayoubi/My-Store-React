78b6a73a190f75b6302e696b5638ea73d9d7c2b6
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-09-27 19:46:35 +0900

    Making six components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">Column Number One</div>
-        <div className="col-6">Column Number Two</div>
-      </div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;

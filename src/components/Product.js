115b1deae207b373cfb4c68ec7bfa9f0d9198377
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-09-29 23:08:33 +0900

    Products update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,1 @@
-import React, { Component } from 'react'
 
-export default class Product extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Hello Form Product</h3>
-            </div>
-        )
-    }
-}

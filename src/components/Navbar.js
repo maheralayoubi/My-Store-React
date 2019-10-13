d8a3bb383a4cff8d0b2cc8d345b5d2762861e1a8
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-10-13 23:36:10 +0900

    Nav-bar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import logo from '../logo.svg'
 export default class Navbar extends Component {
     render() {
         return(
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
                 {/* 
                 https://www.iconfinder.com/icons/1243689/call_phone_icon
                 Creative Commons (Attribution 3.0 Unported);
@@ -13,6 +13,13 @@ export default class Navbar extends Component {
                 <Link to='/'>
                     <img src={ logo } alt="store" className="navbar-brand"/>
                 </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                    <Link to='/' className="nav-link">
+                        Products
+                    </Link>
+                    </li>
+                </ul>
             </nav>
         )
     }

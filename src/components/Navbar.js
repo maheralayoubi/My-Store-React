4908c2b56bb4678034f133b8fe62cd8d63901891
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-10-18 16:28:55 +0900

    Creating a component fot the button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../logo.svg'
 import styled from 'styled-components'
+import { ButtonContainer} from './Button'
 
 export default class Navbar extends Component {
     render() {
@@ -33,24 +34,4 @@ export default class Navbar extends Component {
         )
     }
 }
-
-const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.05rem solid var(--lightBlue);
-  color: var(--lightBlue);
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  cursor: pointer;
-  margin: 0.2rem 0.5rem 0.2rem 0;
-  transition: all 0.5s ease-in-out;
- &:hover {
-     background: var(--lightBlue);
-     color: var(--mainBlue)'
- }
- &:focus {
-    outline: none;
- }
-`;
 
